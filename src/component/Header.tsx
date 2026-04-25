806d8a98e3c3757ce26eb0a65d539efec5c7449c
Ads4331x <karkitan@gmail.com>
2026-04-25 17:35:05 +0545

    first commit

---
+++ src/component/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <img src="../assets/FIS_logo.png" alt="FairyLand Seconday School Logo" />
+      <nav></nav>
+    </header>
+  );
+}
+
+export default Header;
