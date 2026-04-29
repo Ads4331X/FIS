ab271803df8cc662f1b21a6ba1fef08991eb42a9
Ads4331x <karkitan@gmail.com>
2026-04-29 17:15:54 +0545

    added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
+import Footer from "./component/Footer";
 import Header from "./component/Header";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/about_us" element={<h1>About</h1>} />
+        <Route path="/facilities" element={<h1>Facilities</h1>} />
+        <Route path="/academics" element={<h1>Academics</h1>} />
+        <Route path="/news_and_events" element={<h1>News</h1>} />
+        <Route path="/contact" element={<h1>Contact</h1>} />
+      </Routes>
+      <Footer />
     </>
   );
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
