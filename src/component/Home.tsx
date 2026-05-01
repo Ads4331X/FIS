8c14bc8a46d6fae0ce5dfeb7965105cc47d3512b
Ads4331x <karkitan@gmail.com>
2026-05-01 19:54:08 +0545

    added home page

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,53 +1,20 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, A11y, Autoplay } from "swiper/modules";
 import { Box } from "@mui/material";
-
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-
-import imageSliders from "../data/imageSlider.json";
-
+import HomeHero from "./HomeHero";
+import HomeSubContent from "./HomeSubContent";
+import AcademicExcellence from "./AcademicExcellence";
+import ReadyToJoin from "./ReadyToJoin";
 export default function Home() {
   return (
     <Box
       sx={{
-        margin: 0,
         padding: 0,
-        overflow: "hidden",
-        width: "100%",
-        background:
-          "linear-gradient(rgba(13, 42, 110, 0.75),rgba(13, 42, 110, 0.85))",
+        margin: 0,
       }}
     >
-      <Swiper
-        modules={[Navigation, Pagination, A11y, Autoplay]}
-        slidesPerView={1}
-        spaceBetween={0}
-        loop={true}
-        autoplay={{ delay: 3000, disableOnInteraction: false }}
-        navigation
-        pagination={{ clickable: true }}
-        observer={true}
-        observeParents={true}
-        resizeObserver={true}
-        style={{
-          width: "100%",
-        }}
-      >
-        {imageSliders.map((image, key) => (
-          <SwiperSlide key={key}>
-            <img
-              src={image}
-              style={{
-                width: "100%",
-                height: "auto",
-                display: "block", 
-              }}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <HomeHero />
+      <HomeSubContent />
+      <AcademicExcellence />
+      <ReadyToJoin />
     </Box>
   );
 }
