56e7e7c6b19144aa59296070a638c83ef62596a6
Ads4331x <karkitan@gmail.com>
2026-05-02 16:53:31 +0545

    working on about page

---
+++ src/pages/About/About.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+import { AboutHero } from "./AboutHero";
+import { OurMission } from "./OurMission";
+export default function About() {
+  return (
+    <Box>
+      <AboutHero />
+      <OurMission />
+    </Box>
+  );
+}
