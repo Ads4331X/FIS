6f99097aee17d0a51e0963313fe76880a63d1b0c
Ads4331x <karkitan@gmail.com>
2026-05-03 17:31:11 +0545

    added about page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,11 +1,28 @@
 import { Box } from "@mui/material";
 import { AboutHero } from "./AboutHero";
 import { OurMission } from "./OurMission";
+import { EducationalJourney } from "./EducationalJourney";
+import { ScheduleVisit } from "./ScheduleVisit";
 export default function About() {
   return (
     <Box>
       <AboutHero />
       <OurMission />
+      <Box
+        sx={{
+          background: "whitesmoke",
+        }}
+      >
+        <EducationalJourney />
+      </Box>
+      <Box
+        sx={{
+          background: "white",
+          marginBottom: "32px",
+        }}
+      >
+        <ScheduleVisit />
+      </Box>
     </Box>
   );
 }
