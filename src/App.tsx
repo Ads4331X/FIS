3d963739f0e4a0157d6ba5167251ab949fcfc3d7
Ads4331x <karkitan@gmail.com>
2026-05-04 18:43:37 +0545

    linked admission page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Box } from "@mui/material";
-import Footer from "./component/Footer";
-import Header from "./component/Header";
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
 import Home from "./pages/Home/Home";
 import { Routes, Route } from "react-router-dom";
 import About from "./pages/About/About";
 import Academics from "./pages/Academics/Academics";
 import Contact from "./pages/contact/contact";
+import Admission from "./pages/Admission/Admission";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/academics" element={<Academics />} />
         <Route path="/news_and_events" element={<h1>News</h1>} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/apply_now" element={<Admission />} />
       </Routes>
       <Footer />
     </Box>
