9f98f7c72237534363a909c54b21555695157d18
Ads4331x <karkitan@gmail.com>
2026-05-04 18:41:45 +0545

    improved about page

--- src/pages/About/AboutHero.tsx
+++ src/pages/About/AboutHero.tsx
@@ -1,73 +1,18 @@
-import { Box, Container } from "@mui/material";
+import { Hero } from "../../components/ui/Hero";
 
 export function AboutHero() {
   return (
-    <Box
-      sx={{
-        position: "relative",
-        minHeight: "90vh",
-        display: "flex",
-        alignItems: "center",
-        background: "url('/images/about.jpg') center/cover no-repeat",
-
-        "&::before": {
-          content: '""',
-          position: "absolute",
-          inset: 0,
-          background:
-            "linear-gradient(to right, rgba(0,28,58,0.9), rgba(0,28,58,0.6), rgba(0,28,58,0.2))",
-          zIndex: 0,
-        },
-      }}
-    >
-      <Container sx={{ position: "relative", zIndex: 1 }}>
-        <Box sx={{ maxWidth: 600 }}>
-          {/* badge */}
-          <Box
-            sx={{
-              display: "inline-block",
-              backgroundColor: "red",
-              color: "white",
-              px: 2,
-              py: 0.5,
-              borderRadius: "999px",
-              fontSize: "0.75rem",
-              mb: 2,
-              fontWeight: "bold",
-              letterSpacing: "0.1em",
-            }}
-          >
-            EST. 2009
-          </Box>
-
-          {/* title */}
-          <Box
-            component="h1"
-            sx={{
-              color: "white",
-              fontSize: { xs: "2.2rem", md: "3.5rem" },
-              fontWeight: 800,
-              lineHeight: 1.1,
-              mb: 3,
-            }}
-          >
-            Nurturing Minds, <br /> Chasing Excellence.
-          </Box>
-
-          {/* text */}
-          <Box
-            component="p"
-            sx={{
-              color: "#D4E3FF",
-              fontSize: "1rem",
-              maxWidth: 500,
-            }}
-          >
-            For over 15 years, Fairyland Secondary School has been a beacon of
-            academic brilliance and character development.
-          </Box>
-        </Box>
-      </Container>
-    </Box>
+    <Hero
+      bgImg="/images/about.jpg"
+      badge="EST. 2009"
+      title={
+        <>
+          Nurturing Minds,
+          <br />
+          Chasing Excellence.
+        </>
+      }
+      description="For over 15 years, Fairyland Secondary School has been a beacon of academic brilliance and character development."
+    />
   );
 }
